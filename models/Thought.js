ed0925c85a889944740942dc22c4c2a4e0944758
Jake P <76881086+jbped@users.noreply.github.com>
2021-08-20 10:55:48 -0600

    thought schema created

--- models/Thought.js
+++ models/Thought.js
@@ -16,11 +16,20 @@ const reactionSchema = new Schema(
 
 const thoughtSchema = new Schema(
     {
-        thoughtText: {},
-        createdAt: {
-            type: Date
+        thoughtText: {
+            type: String,
+            required: "Please provide a sacrificial body for your thought -- I mean a body of text.",
+            min: [1, "A minimum of one character is required"],
+            max: [280, "A maximum of 280 characters are allowed"]
         },
-        username: {},
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: "Please provide a username"
+        },
         reactions: [reactionSchema]
     },
     {
@@ -32,7 +41,7 @@ const thoughtSchema = new Schema(
 )
 
 thoughtSchema.virtual("reactionCount").get(function(){
-
+    return this.reactions.length;
 })
 
 const Thought = model ("Thought", thoughtSchema);
